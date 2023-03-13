3a7ed10b93ab8c9bda6496d8d75aaf4ac5d5adab
Alisa <alisa.luoma@hotmail.fi>
2023-03-13 17:54:59 +0200

    Add data

--- src/Components/CyclOPediaClassPage.jsx
+++ src/Components/CyclOPediaClassPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getRandomUser } from "../Utility/api";
+import Instructor from "./Instructor";
 
 class CyclOPediaPage extends React.Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class CyclOPediaPage extends React.Component {
       localStorage.getItem("cylcopediaState")
     ) || {
       instructor: undefined,
-      studetList: [],
+      studentList: [],
       studentCount: 0,
       hideInstructor: false,
       inputName: "",
@@ -17,6 +18,7 @@ class CyclOPediaPage extends React.Component {
   }
 
   componentDidMount = async () => {
+    console.log(JSON.parse(localStorage.getItem("cylcopediaState")));
     console.log("Component did mount");
     if (!JSON.parse(localStorage.getItem("cylcopediaState"))) {
       const response = await getRandomUser();
@@ -36,13 +38,38 @@ class CyclOPediaPage extends React.Component {
     }
   };
 
-  componentDidUpdate() {
+  componentDidUpdate = async (previousProps, previousState) => {
     console.log("Component did Update");
     localStorage.setItem(
       "cylcopediaState",
       JSON.stringify(this.state)
     );
-  }
+
+    console.log("old state - " + previousState.studentCount);
+    console.log("new state - " + this.state.studentCount);
+    if (previousState.studentCount < this.state.studentCount) {
+      const response = await getRandomUser();
+      this.setState((prevState) => {
+        return {
+            studentList: [
+              ...prevState.studentList,
+              {
+                name:
+                  response.data.first_name +
+                  " " +
+                  response.data.last_name,
+              },
+            ],
+        };
+      });
+    } else if (previousState.studentCount > this.state.studentCount) {
+      this.setState((prevState) => {
+        return {
+          studentList: [],
+        };
+      });
+    }
+  };
 
   componentWillUnmount() {
     console.log("Component will unmount");
@@ -64,20 +91,32 @@ class CyclOPediaPage extends React.Component {
     });
   };
 
+  handletpggleInstructor = () => {
+    this.setState((prevState) => {
+      return {
+        hideInstructor: !prevState.hideInstructor,
+      };
+    });
+  };
+
   render() {
     console.log("Render Component");
     return (
       <div>
-        {this.state.instructor && (
-          <div className="p-3">
-            <span className="h4 text-success">Instructor</span>
-            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
-            <br />
-            Name: {this.state.instructor.name} <br />
-            Email: {this.state.instructor.email} <br />
-            Phone: {this.state.instructor.phone} <br />
-          </div>
-        )}
+        <div className="p-3">
+          <span className="h4 text-success">Instructor</span>
+          <i
+            className={`bi ${
+              this.state.hideInstructor
+                ? "bi-toggle-off"
+                : "bi-toggle-on"
+            } btn btn-success btn-sm m-1`}
+            onClick={this.handletpggleInstructor}
+          ></i>
+          {!this.state.hideInstructor && this.state.instructor ? (
+            <Instructor instructor={this.state.instructor} />
+          ) : null}
+        </div>
         <div className="p-3">
           <span className="h4 text-successs">Feedback</span>
           <br />
@@ -119,6 +158,16 @@ class CyclOPediaPage extends React.Component {
           >
             Remove All Students
           </button>
+          {this.state.studentList.map((student, index) => {
+            return (
+              <div
+                className="text-white"
+                key={index}
+              >
+                - {student.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     );
---
+++ src/Components/Instructor.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Instructor extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidUpdate() {
+    console.log("Update - Instructor");
+  }
+
+  componentDidMount() {
+    console.log("Mounted - Instructor");
+  }
+
+  componentWillUnmount() {
+    console.log("UnMount - Instructor");
+  }
+
+  render() {
+    console.log("Render - Instructor " + this.props.instructor);
+    return (
+      <div className="p-1">
+        Name: {this.props.instructor.name} <br />
+        Email: {this.props.instructor.email} <br />
+        Phone: {this.props.instructor.phone} <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default Instructor;

c6f8543820a39b78df3df95cbade134b2d2fabd9
Alisa <alisa.luoma@hotmail.fi>
2023-03-13 17:11:50 +0200

    Add data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CyclOPediaPage from './Components/CyclOPediaClassPage';
 import Header from './Components/Layout/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header/>
+  <div>
+    <Header/>
+    <div className='row text-white'>
+      <div className='col-6'>
+        <span className='h1 text-warning text-center'>Class Component</span>
+        <CyclOPediaPage/>
+      </div>
+    </div>
+  </div>
 );
 
